dc394739ddb552a225cdd52976fb602e426c47b0
IHaveTheSourceCode <marcinbureta@gmail.com>
2022-10-18 18:58:09 +0200

    fixed createSound function

--- createSounds.js
+++ createSounds.js
@@ -1,16 +1,16 @@
 // url must be string
 function createSound(url) {
   return function () {
-    return new Audio("public/sounds/" + url);
+    return new Audio("./" + url);
   };
 }
 
-const buttonClick = createSound("../sounds/btn-click.mp3");
-const candyPositive1 = createSound("../sounds/positive1.mp3");
-const candyPositive2 = createSound("../sounds/positive2.mp3");
-const candyNegative = createSound("../sounds/bad-candy.wav");
-const gameOver = createSound("../sounds/game-over.wav");
-const mainTheme = createSound("../sounds/game-theme.mp3");
+const buttonClick = createSound("btn-click.mp3");
+const candyPositive1 = createSound("positive1.mp3");
+const candyPositive2 = createSound("positive2.mp3");
+const candyNegative = createSound("bad-candy.wav");
+const gameOver = createSound("game-over.wav");
+const mainTheme = createSound("game-theme.mp3");
 
 export {
   buttonClick,
