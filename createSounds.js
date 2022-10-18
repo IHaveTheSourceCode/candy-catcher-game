9c07d07182af0f3e2ba22d660ffa73456071acd9
IHaveTheSourceCode <marcinbureta@gmail.com>
2022-10-18 21:47:02 +0200

    updated main script path

--- createSounds.js
+++ createSounds.js
@@ -5,12 +5,12 @@ function createSound(url) {
   };
 }
 
-// const buttonClick = createSound("btn-click.mp3");
-// const candyPositive1 = createSound("positive1.mp3");
-// const candyPositive2 = createSound("positive2.mp3");
-// const candyNegative = createSound("bad-candy.wav");
-// const gameOver = createSound("game-over.wav");
-// const mainTheme = createSound("game-theme.mp3");
+const buttonClick = createSound("btn-click.mp3");
+const candyPositive1 = createSound("positive1.mp3");
+const candyPositive2 = createSound("positive2.mp3");
+const candyNegative = createSound("bad-candy.wav");
+const gameOver = createSound("game-over.wav");
+const mainTheme = createSound("game-theme.mp3");
 
 export {
   buttonClick,
